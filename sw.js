59eb192cbdfee02d64478075c1242275995457f2
divppc <okal.ihor@gmail.com>
2018-01-29 16:47:47 +0200

    sw scripts moved to root folder

---
+++ sw.js
@@ -0,0 +1,2 @@
+‘use strict’;
+importScripts(‘sw-toolbox.js’); toolbox.precache([“index.html”,assets/css/style.css”]); toolbox.router.get(‘assets/images/*’, toolbox.cacheFirst); toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
