2d2656a2c4d2c8cb2c60417c6e87a102ce50abef
divppc <okal.ihor@gmail.com>
2018-01-29 17:02:50 +0200

    fucking sw2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ‘use strict’;
 
-importScripts(‘sw-toolbox.js’);
+importScripts('sw-toolbox.js');
 toolbox.precache(['index.html','assets/css/style.css']);
 toolbox.router.get('assets/images/*', toolbox.cacheFirst);
 toolbox.router.get('*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
