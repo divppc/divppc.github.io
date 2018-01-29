d38418a2a9c4cd6dcd3924a193d0c1ba4b1522ca
divppc <okal.ihor@gmail.com>
2018-01-29 16:57:17 +0200

    another qoute changed

--- sw.js
+++ sw.js
@@ -3,4 +3,4 @@
 importScripts(‘sw-toolbox.js’);
 toolbox.precache(['index.html','assets/css/style.css']);
 toolbox.router.get('assets/images/*', toolbox.cacheFirst);
-toolbox.router.get(‘/*’, toolbox.networkFirst, { networkTimeoutSeconds: 5});
+toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
